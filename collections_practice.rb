3be5d02f8c2ade3345a16377e88e62b466267d89
Joshua Suskin <jsuskin85@gmail.com>
2019-05-10 20:45:21 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -60,5 +60,32 @@ def merge_data(h1, h2)
   end
 end
 
+def find_cool(data)
+  arr = []
+  data.each do |h|
+    if h[:temperature] == "cool"
+      arr << h
+    end
+  end
+  arr
+end
+
+def organize_schools(schools)
+  new_hash = {}
+  schools.each do |k, v|
+    if !new_hash.key?(v[:location])
+      new_hash[v[:location]] = []
+    end
+  end
+  new_hash.each do |location,arr|
+    schools.select {|school, obj| obj[:location] == location}.keys.each do |t|
+        arr << t
+      end
+  end
+  new_hash
+end
+
 #count_elements([{:name => "blake"}, {:name => "blake"}, {:name => "ashley"}])
 #merge_data([{:first_name=>"blake", :motto=>"Have a koala-ty day!"}, {:first_name=>"ashley", :motto=>"I dub thee, 'Lady Brett Ashley'."}], [{"ashley"=>{:awesomeness=>9, :height=>60, :last_name=>"dubs"}, "blake"=>{:awesomeness=>10, :height=>"74", :last_name=>"johnson"}}])
+#find_cool([{:name=>"ashley", :temperature=>"sort of cool"}, {:name=>"blake", :temperature=>"cool"}])
+organize_schools({"Hack Reactor"=>{:location=>"SF"}, "dev boot camp"=>{:location=>"SF"}, "dev boot camp chicago"=>{:location=>"NYC"}, "flatiron school bk"=>{:location=>"NYC"}, "general assembly"=>{:location=>"NYC"}})
